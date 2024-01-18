485e4574f516983670c8072185e7084db004c492
LeonardoASC <leonardoaschaves@gmail.com>
2024-01-18 15:40:19 -0300

    vai porra

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-yellow-800">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
