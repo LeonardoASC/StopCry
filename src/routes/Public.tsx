957c1b0732b45996c1d7b91a282bb597b08440ed
LeonardoASC <leonardoaschaves@gmail.com>
2025-02-18 18:42:07 -0300

    feat: atualizar rotas e importar máscara SVG

--- src/routes/Public.tsx
+++ src/routes/Public.tsx
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 export default function Public() {
   return (
     <Stack.Navigator
-    initialRouteName="Login"
+    initialRouteName="Presentation"
       screenOptions={{
         headerShown: false
       }}
